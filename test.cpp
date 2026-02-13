f3f59237d1d478a78ca603a8e3cfff16fcfac33f
Bhuvan S Shetty <135850214+BhuvanSShetty@users.noreply.github.com>
2026-02-14 01:23:30 +0530

    branch new1 first commit

--- test.cpp
+++ test.cpp
@@ -1,2 +1,16 @@
 #include <iso646.h>
 
+int main() {
+    int a = 5;
+    int b = 10;
+
+    if (a < b) {
+        // Do something
+    }
+
+    if (a > b) {
+        // Do something else
+    }
+
+    return 0;
+}
