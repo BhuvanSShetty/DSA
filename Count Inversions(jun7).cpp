d2d9d25b32ed31026d54809b8657f3ceb904ce08
Bhuvan S Shetty <135850214+BhuvanSShetty@users.noreply.github.com>
2026-06-10 01:54:31 +0530

    Refactor Count Inversions algorithm to improve clarity and efficiency

--- Count Inversions(jun7).cpp
+++ Count Inversions(jun7).cpp
@@ -1,93 +1,123 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <climits>
-#include <cmath>
-#include <numeric>
-#include <deque>
-
 using namespace std;
+
 class Solution {
 public:
 
-    long long merge(vector<int>& arr, int low, int mid, int high) {
-
+    void merge(vector<int>& arr, int low, int mid, int high)
+    {
         vector<int> temp;
 
         int left = low;
         int right = mid + 1;
 
-        long long cnt = 0;
-
-        while (left <= mid && right <= high) {
-
-            if (arr[left] <= arr[right]) {
+        while(left <= mid && right <= high)
+        {
+            if(arr[left] <= arr[right])
+            {
                 temp.push_back(arr[left]);
                 left++;
             }
-            else {
-
-                cnt += (mid - left + 1);
-
+            else
+            {
                 temp.push_back(arr[right]);
                 right++;
             }
         }
 
-        while (left <= mid) {
+        while(left <= mid)
+        {
             temp.push_back(arr[left]);
             left++;
         }
 
-        while (right <= high) {
+        while(right <= high)
+        {
             temp.push_back(arr[right]);
             right++;
         }
 
-        for (int i = low; i <= high; i++) {
+        for(int i = low; i <= high; i++)
+        {
             arr[i] = temp[i - low];
+        }
+    }
+
+    long long checkInversion(vector<int>& arr,
+                             int low,
+                             int mid,
+                             int high)
+    {
+        long long cnt = 0;
+
+        int left = low;
+        int right = mid + 1;
+
+        while(left <= mid && right <= high)
+        {
+            if(arr[left] <= arr[right])
+            {
+                left++;
+            }
+            else
+            {
+                cnt += (mid - left + 1);
+                right++;
+            }
         }
 
         return cnt;
     }
 
-    long long mergeSort(vector<int>& arr, int low, int high) {
-
-        if (low >= high)
+    long long mergeSort(vector<int>& arr,
+                        int low,
+                        int high)
+    {
+        if(low >= high)
             return 0;
 
-        int mid = (low + high) / 2;
-
         long long cnt = 0;
+
+        int mid = low + (high - low) / 2;
 
         cnt += mergeSort(arr, low, mid);
 
         cnt += mergeSort(arr, mid + 1, high);
 
-        cnt += merge(arr, low, mid, high);
+        cnt += checkInversion(arr, low, mid, high);
+
+        merge(arr, low, mid, high);
 
         return cnt;
     }
 
-    long long int numberOfInversions(vector<int> nums) {
-
+    long long numberOfInversions(vector<int> nums)
+    {
         int n = nums.size();
 
         return mergeSort(nums, 0, n - 1);
     }
 };
 
-int main() {
+int main()
+{
     Solution s;
+
     vector<int> nums = {1, 20, 6, 4, 5};
-    long long inversions = s.numberOfInversions(nums);
-    cout << "Number of Inversions: " << inversions << endl;     
+
+    cout << "Array: ";
+
+    for(int x : nums)
+    {
+        cout << x << " ";
+    }
+
+    cout << endl;
+
+    cout << "Number of Inversions = "
+         << s.numberOfInversions(nums)
+         << endl;
+
     return 0;
 }
