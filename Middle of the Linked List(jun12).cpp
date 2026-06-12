d5ba56f125f71f2fe529c4beb4729cd9573e1b67
Bhuvan S Shetty <135850214+BhuvanSShetty@users.noreply.github.com>
2026-06-12 21:49:23 +0530

    Add implementation of Middle of the Linked List algorithm

---
+++ Middle of the Linked List(jun12).cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <climits>
+#include <cmath>
+#include <numeric>
+#include <deque>
+
+using namespace std;
+
+
+  struct ListNode {
+      int val;
+     ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    int countLinkedList(ListNode* head)
+    {
+        int cnt = 0;
+        while(head)
+        {
+            head=head->next;
+            cnt++;
+        }
+        return cnt;
+    }
+    bool isEven(int number) {
+    return number % 2 == 0;
+}
+    ListNode* middleNode(ListNode* head) {
+        int cnt = countLinkedList(head);
+        cnt=cnt/2;
+        if(cnt==0)
+            return head;
+        ListNode *temp = nullptr;
+        while(cnt--)
+        {
+            temp = head->next;
+            head=head->next;
+        }
+        return temp;
+    }
+};
+
+int main() {
+    Solution s;
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    int cur = s.middleNode(head)->val;
+    cout<<"Middle node value is "<<cur;
+    
+
+    return 0;
+}
